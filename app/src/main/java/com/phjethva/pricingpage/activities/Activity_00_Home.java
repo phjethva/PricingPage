b2e97ab044e1efab1bc8fc9b6bfa7a3c9dbae3ae
Pratik Jethva <phjethva@gmail.com>
2018-06-03 22:33:22 +0530

    Project structure created.

---
+++ app/src/main/java/com/phjethva/pricingpage/activities/Activity_00_Home.java
@@ -0,0 +1,16 @@
+package com.phjethva.pricingpage.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.phjethva.pricingpage.R;
+
+public class Activity_00_Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_00_home);
+    }
+
+}
